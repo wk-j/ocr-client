6f11f366e9abdad6fb9ed38a36caecf95f07815b
wk-j <somnuk.wk@gmail.com>
2019-11-19 19:22:29 +0700

    Update client API

--- src/main/java/wk/Program.java
+++ src/main/java/wk/Program.java
@@ -1,20 +1,40 @@
 package wk;
 
 import bcircle.ProImage;
+import bcircle.Profile;
 import bcircle.SendImage;
+import bcircle.SendProfile;
 
 public class Program {
     public static void main(String[] args) throws Exception {
 
-        SendImage service = new SendImage("http://localhost:3000");
+        String url = "http://localhost:3000";
+        String profileName = "invoice";
 
-        String result = service.postImage(new ProImage() {
-            {
-                setProfile("profile");
-                setPartimage("http/invoice.jpg");
-            }
-        });
+        {
+            SendProfile profileClient = new SendProfile(url);
+            Profile profile = new Profile() {
+                {
+                    setProfile(profileName);
+                    setX(0);
+                    setY(0);
+                    setWidth(5);
+                    setHeight(5);
+                }
+            };
+            profileClient.postProfile(profile);
+        }
 
-        System.out.println("result: " + result);
+        {
+            SendImage service = new SendImage(url);
+            String result = service.postImage(new ProImage() {
+                {
+                    setProfile(profileName);
+                    setPartImage("/Users/wk/GitHub/OcrClient/http/invoice.jpg");
+                }
+            });
+
+            System.out.println("result: " + result);
+        }
     }
 }
